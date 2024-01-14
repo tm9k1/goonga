17f847535d4fb298684fadc4ef69589926a9bd2e
Piyush Aggarwal <piyushaggarwal002@gmail.com>
2024-01-14 22:20:11 +0530

    copy concept art alongside binary at config time

--- src/imageprovider/ImageProvider.cpp
+++ src/imageprovider/ImageProvider.cpp
@@ -1,11 +1,11 @@
 #include "ImageProvider.h"
-
+#include <QCoreApplication>
 
 QImage ImageProvider::requestImage(const QString &id, QSize *size, const QSize &requestedSize) {
 
     int width = 300;
     int height = 300;
-    QImage image = QImage("/home/tm9k1/dev/repos/goonga/goonga/assets/concept.jpg")
+    QImage image = QImage(qApp->applicationDirPath() + "/concept.jpg")
                     .scaled(
                         requestedSize.width() > 0 ? requestedSize.width() : width,
                         requestedSize.height() > 0 ? requestedSize.height() : height,
